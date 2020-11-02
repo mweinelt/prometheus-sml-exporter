855c4c48457facab1d93f73dae4d4515c5ed1248
Martin Weinelt <hexa@darmstadt.ccc.de>
2020-11-02 22:29:08 +0100

    __main__: set default port allocation
    some click nicities and black.

--- prometheus_sml_exporter/__main__.py
+++ prometheus_sml_exporter/__main__.py
@@ -6,16 +6,27 @@ from sml.asyncio import SmlProtocol
 
 from . import SmlExporter
 
+
 @click.command()
-@click.argument('tty', default='/dev/ttyUSB0')
-@click.option('--http-port', '-p', default=9999)
-def main(tty, http_port):
+@click.argument(
+    "tty",
+    default="/dev/ttyUSB0",
+    type=click.Path(exists=True),
+    help="Path to the D0 serial device",
+)
+@click.option(
+    "--http-port",
+    "-p",
+    type=click.IntRange(1024, 65535),
+    default=9761,
+    show_default=True,
+    help="HTTP Port for the Prometheus Exporter",
+)
+def main(tty: str, http_port: int) -> None:
     handler = SmlExporter()
     proto = SmlProtocol(tty)
-    proto.add_listener(handler.event, ['SmlGetListResponse'])
+    proto.add_listener(handler.event, ["SmlGetListResponse"])
     start_http_server(http_port)
     loop = asyncio.get_event_loop()
     loop.run_until_complete(proto.connect(loop))
     loop.run_forever()
-
-
